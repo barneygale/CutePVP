e9eb26f9457a98958f07d4be09675db4c46e63e8
Stephen Mcgregor <tehborris@gmail.com>
2012-04-13 20:14:04 +1000

    Initial code commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## CutePVP ##
+
+A team based PVP plugin for the minecraft bukkit server
+
+> More readme needs to be added
+
---
+++ src/com/c45y/CutePVP/CutePVP.java
@@ -0,0 +1,218 @@
+package com.c45y.CutePVP;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class CutePVP extends JavaPlugin {
+	private final CutePVPListener loglistener = new CutePVPListener(this);
+	Logger log = Logger.getLogger("Minecraft");
+
+	@Override
+	public void onEnable() {
+		this.getConfig().options().copyDefaults(true);
+		// Red spawn
+		this.getConfig().addDefault("spawn.red.x", getServer().getWorlds().get(0).getSpawnLocation().getX());
+		this.getConfig().addDefault("spawn.red.y", getServer().getWorlds().get(0).getSpawnLocation().getY());
+		this.getConfig().addDefault("spawn.red.z", getServer().getWorlds().get(0).getSpawnLocation().getZ());
+		// Blue spawn
+		this.getConfig().addDefault("spawn.blue.x", getServer().getWorlds().get(0).getSpawnLocation().getX());
+		this.getConfig().addDefault("spawn.blue.y", getServer().getWorlds().get(0).getSpawnLocation().getY());
+		this.getConfig().addDefault("spawn.blue.z", getServer().getWorlds().get(0).getSpawnLocation().getZ());
+		// Yellow spawn
+		this.getConfig().addDefault("spawn.yellow.x", getServer().getWorlds().get(0).getSpawnLocation().getX());
+		this.getConfig().addDefault("spawn.yellow.y", getServer().getWorlds().get(0).getSpawnLocation().getY());
+		this.getConfig().addDefault("spawn.yellow.z", getServer().getWorlds().get(0).getSpawnLocation().getZ());
+		// Green spawn
+		this.getConfig().addDefault("spawn.green.x", getServer().getWorlds().get(0).getSpawnLocation().getX());
+		this.getConfig().addDefault("spawn.green.y", getServer().getWorlds().get(0).getSpawnLocation().getY());
+		this.getConfig().addDefault("spawn.green.z", getServer().getWorlds().get(0).getSpawnLocation().getZ());
+		// Block of power
+		this.getConfig().addDefault("block.buff.x", getServer().getWorlds().get(0).getSpawnLocation().getX());
+		this.getConfig().addDefault("block.buff.y", getServer().getWorlds().get(0).getSpawnLocation().getY());
+		this.getConfig().addDefault("block.buff.z", getServer().getWorlds().get(0).getSpawnLocation().getZ());
+
+		this.getConfig().addDefault("base.protection.radius", 50);
+
+		saveConfig();
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(loglistener, this);
+		System.out.println(this.toString() + " enabled");
+
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+			public void run() {
+				getLogger().info("Running buff");
+				Location powerblock = new Location(
+						getServer().getWorlds().get(0),
+						getConfig().getDouble("block.buff.x"),
+						getConfig().getDouble("block.buff.y"),
+						getConfig().getDouble("block.buff.z"));
+				if (getServer().getWorlds().get(0).getBlockAt(powerblock) != null) {
+					Block gPowerBlock = getServer().getWorlds().get(0).getBlockAt(powerblock);
+					if (gPowerBlock.getType() == Material.WOOL) {
+						String winTeam = null;
+						if (gPowerBlock.getData() == (short) 14) {
+							winTeam = "red";
+						} else if (gPowerBlock.getData() == (short) 3) {
+							winTeam = "blue";
+						} else if (gPowerBlock.getData() == (short) 4) {
+							winTeam = "yellow";
+						} else if (gPowerBlock.getData() == (short) 5) {
+							winTeam = "green";
+						}
+						if (winTeam != null) {
+							System.out.println("[NOTICE] " + winTeam + " gets buff!");
+							for (Player playeri : getServer().getOnlinePlayers()) {
+								if (teamName(playeri.getName()) == winTeam) {
+									playeri.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
+									playeri.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 0));
+								}
+							}
+						}
+					}
+				}
+				getLogger().info("End running buff");
+			}
+		}, 1200, 12000);
+	}
+
+	public void onDisable() {
+		log.info("playzorz disabled.");
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Sorry, this plugin cannot be used from console");
+			return true;
+		}
+		Player player = (Player) sender;
+		Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+		if (command.getName().equalsIgnoreCase("asdf")) {
+			if (args[0] != null) {
+				getConfig().set(args[0] + ".x", block.getLocation().getX());
+				getConfig().set(args[0] + ".y", block.getLocation().getY());
+				getConfig().set(args[0] + ".z", block.getLocation().getZ());
+			} else {
+				getConfig().set("block.buff.x", block.getLocation().getX());
+				getConfig().set("block.buff.y", block.getLocation().getY());
+				getConfig().set("block.buff.z", block.getLocation().getZ());
+			}
+			saveConfig();
+			player.sendMessage("Set to: " + block.getType().toString());
+			return true;
+		}
+		return false;
+	}
+
+	public int getTeam(String inpt) {
+		return (Character.getNumericValue(ChatColor.stripColor(inpt).charAt(0)) % 4);
+	}
+
+	public Location getRespawnTeamLocation(String playerName) {
+		String team = teamName(playerName);
+		return getRespawnTeamLocationByTeam(team);
+	}
+
+	public Location getRespawnTeamLocationByTeam(String teamName) {
+		return new Location(
+				getServer().getWorlds().get(0),
+				getConfig().getInt("spawn." + teamName + ".x"),
+				getConfig().getInt("spawn." + teamName + ".y"),
+				getConfig().getInt("spawn." + teamName + ".z"));
+	}
+
+	public boolean getInRangeOfEnemyTeamSpawn(Player player) {
+		int rad = getConfig().getInt("base.protection.radius");
+		Location player_loc = player.getLocation();
+		if (teamName(player.getName()) != "red") {
+			Location _spawn = getRespawnTeamLocationByTeam("red");
+			double dx = _spawn.getX() - player_loc.getX();
+			double dz = _spawn.getZ() - player_loc.getZ();
+			if ((dx<rad && dx>-rad) && (dz<rad && dz>-rad)) {
+				return true;
+			}
+		} else if (teamName(player.getName()) != "blue") {
+			Location _spawn = getRespawnTeamLocationByTeam("blue");
+			double dx = _spawn.getX() - player_loc.getX();
+			double dz = _spawn.getZ() - player_loc.getZ();
+			if ((dx<rad && dx>-rad) && (dz<rad && dz>-rad)) {
+				return true;
+			}
+		} else if (teamName(player.getName()) != "yellow") {
+			Location _spawn = getRespawnTeamLocationByTeam("yellow");
+			double dx = _spawn.getX() - player_loc.getX();
+			double dz = _spawn.getZ() - player_loc.getZ();
+			if ((dx<rad && dx>-rad) && (dz<rad && dz>-rad)) {
+				return true;
+			}
+		} else if (teamName(player.getName()) != "green") {
+			Location _spawn = getRespawnTeamLocationByTeam("green");
+			double dx = _spawn.getX() - player_loc.getX();
+			double dz = _spawn.getZ() - player_loc.getZ();
+			if ((dx<rad && dx>-rad) && (dz<rad && dz>-rad)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public short woolColor(String inpt) {
+		int ret = getTeam(inpt);
+		switch (ret) {
+		case 0: return 14; // Red
+		case 1: return 3;  // Blue
+		case 2: return 4;  // Yellow
+		case 3: return 5;  // Green
+		}
+		return (short) ret;
+	}
+
+	public String teamName(String inpt) {
+		int ret = getTeam(inpt);
+		return teamNameFromInt(ret);
+	}
+
+	public String teamNameFromInt(int ret) {
+		switch (ret) {
+		case 0: return "red";
+		case 1: return "blue";
+		case 2: return "yellow";
+		case 3: return "green";
+		}
+		return "";
+	}
+
+	public ItemStack returnWool(String inpt) {
+		return new ItemStack(Material.WOOL.getId(), 1, woolColor(inpt));
+	}
+
+	public String colorName(String inpt) {
+		int ret = getTeam(inpt);
+		String retName = "";
+		switch (ret) {
+		case 0: retName += ChatColor.RED;
+		break;
+		case 1: retName += ChatColor.BLUE;
+		break;
+		case 2: retName += ChatColor.YELLOW;
+		break;
+		case 3: retName += ChatColor.GREEN;
+		break;
+		}
+		retName += (inpt + ChatColor.WHITE);
+		return retName;
+	}
+}
